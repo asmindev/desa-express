06bf07803b97de574aaddd585da9eda6833fda9d
asmindev <asmin987asmin@gmail.com>
2022-11-12 23:27:12 +0800

    fix env file

--- database/conn.js
+++ database/conn.js
@@ -3,13 +3,18 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 
 dotenv.config()
-mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}, (err) => {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log('connected to database')
-    }
-})
+const URI = process.env.MONGO_URI
+if (!URI) {
+    throw new Error('You must provide a MONGO_URI in .env file')
+} else {
+    mongoose.connect(URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }, (err) => {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log('connected to database')
+        }
+    })
+}
